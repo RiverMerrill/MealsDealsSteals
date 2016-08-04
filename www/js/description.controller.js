5111d37849b212aef1759047e4333052d561ff7b
River Merrill <rivermerrill.me@gmail.com>
2016-08-04 19:32:53 +0000

    added redeemed counter

--- www/js/description.controller.js
+++ www/js/description.controller.js
@@ -5,10 +5,19 @@
     .module('DealsApp')
     .controller('DescriptionController', DescriptionController);
 
-  function DescriptionController($scope, $state, $stateParams, $firebaseObject, $localStorage) {
+  function DescriptionController($scope, $state, $stateParams, $firebaseObject, $firebaseArray, $localStorage) {
     var ref = new Firebase('https://mealsdealssteals.firebaseio.com/coupons/' + $stateParams.id);
+    var ref2 = new Firebase('https://mealsdealssteals.firebaseio.com/');
+    var redeemed;
+
     $scope.coupon = $firebaseObject(ref)
+    console.log($scope.coupon);
     $scope.redeem = function () {
+      ref2.child('redeemed').once("value", function(snapshot) {
+      redeemed = snapshot.val();
+      console.log(snapshot.val())
+      ref2.update({redeemed: redeemed + 1});
+    });
       if ($scope.coupon.oneTime === "true") {
         $localStorage.IDs.push($scope.coupon.$id);
       }
