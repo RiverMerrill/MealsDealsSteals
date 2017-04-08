eea3fd35a3e1454d20258bf702c1d8aa2b96d371
River Merrill <rivermerrill.me@gmail.com>
2017-04-08 02:08:04 +0000

    working on analytics, redeem initializes but does not update - unfinished

--- www/js/description.controller.js
+++ www/js/description.controller.js
@@ -7,23 +7,51 @@
 
   function DescriptionController($scope, $state, $stateParams, $firebaseObject, $firebaseArray, $localStorage) {
     var ref = new Firebase('https://mealsdealssteals.firebaseio.com/coupons/' + $stateParams.id);
-    var ref2 = new Firebase('https://mealsdealssteals.firebaseio.com/');
     var redeemed;
+    var d = new Date();
+    var m = d.getMonth();
+    var ref2 = new Firebase('https://mealsdealssteals.firebaseio.com/');
     $scope.coupon = $firebaseObject(ref)
     console.log($scope.coupon);
     $scope.redeem = function () {
-      ref.once("value", function(snapshot) {
+      ref.once('value', function(snapshot) {
       if(snapshot.val().redeemed == undefined){
           ref.update({redeemed: 1});
       } else{
           redeemed = snapshot.val().redeemed;
           ref.update({redeemed: redeemed + 1});
       }
-      console.log(snapshot.val().redeemed)
-    });
+        console.log(snapshot.val().redeemed)
+      });
       if ($scope.coupon.oneTime === "true") {
         $localStorage.IDs.push($scope.coupon.$id);
       }
+      ref2.once('value').then(function(snapshot){
+        if(!snapshot.val()[m]){
+          debugger;
+          var updates = {};
+          updates[$scope.coupon.title] = 1;
+          firebase.database().ref(m).set({})
+          firebase.database().ref(m + '/' + $scope.coupon.company).set({})
+          firebase.database().ref(m + '/' + $scope.coupon.company).set(updates);
+        } else if (!snapshot.val()[m][$scope.coupon.company]){
+          debugger;
+          var updates = {};
+          updates[$scope.coupon.title] = 1;
+          firebase.database().ref(m + '/' + $scope.coupon.company).set(updates);
+        } else if (!snapshot.val()[m][$scope.coupon.company][$scope.coupon.title]){
+          debugger;
+          var updates = {}
+          updates[$scope.coupon.title] = 1;
+          firebase.database().ref(m + '/' + $scope.coupon.company).update(updates)
+        } else {
+          debugger;
+          var redeemed = snapshot.val()[m][$scope.coupon.company][$scope.coupon.title];
+          var updates = {};
+          updates[$scope.coupon.title] = redeemed;
+          firebase.database().ref(m + '/' + $scope.coupon.company).update(updates)
+        }
+      })
       $state.go('coupon', {image: $scope.coupon.image})
     }
   }
